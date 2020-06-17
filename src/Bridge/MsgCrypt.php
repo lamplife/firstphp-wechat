1b3c21a90fd010f4d0225724814fad08696133b2
狂奔的螞蟻 <firstphp@firstphp.com>
2020-06-17 23:24:15 +0800

    支持客服消息推送

--- src/Bridge/MsgCrypt.php
+++ src/Bridge/MsgCrypt.php
@@ -11,14 +11,15 @@ declare(strict_types = 1);
 namespace Firstphp\Fpwechat\Bridge;
 
 
-class MsgCrypt
-{
+class MsgCrypt {
 
     protected $token;
     protected $encodingAesKey;
     protected $appId;
+    protected $k;
 
     const BLOCK_SIZE = 32;
+
     const OK = 0;
     const VALIDATE_SIGNATURE_ERROR = -40001;
     const PARSE_XML_ERROR = -40002;
@@ -32,7 +33,6 @@ class MsgCrypt
     const DECODE_BASE64_ERROR = -40010;
     const GEN_RETURN_XML_ERROR = -40011;
 
-
     /**
      * 构造函数
      *
@@ -45,8 +45,8 @@ class MsgCrypt
         $this->token = $token;
         $this->encodingAesKey = $encodingAesKey;
         $this->appId = $appId;
+        $this->key = base64_decode($encodingAesKey . "=");
     }
-
 
     /**
      * 消息加密
@@ -55,147 +55,100 @@ class MsgCrypt
      * @param $timeStamp
      * @param $nonce
      */
-    public function encryptMsg($replyMsg, $timeStamp = null, $nonce = null)
+    public function encryptMsg($replyMsg, $timestamp = null, $nonce = null)
     {
+        //加密
         $array = $this->encrypt($replyMsg, $this->appId);
         if ($array[0] != 0) {
             return $array;
         }
-        $timeStamp = $timeStamp ?: time();
+        $timestamp = $timestamp ? : time();
         $encrypt = $array[1];
-        $array = $this->_getSHA1($this->token, $timeStamp, $nonce, $encrypt);
+
+        //生成安全签名
+        $array = $this->_getSHA1($this->token, $timestamp, $nonce);
         if ($array[0] != 0) {
             return $array;
         }
         $signature = $array[1];
-        $encryptMsg = $this->_generateXml($encrypt, $signature, $timeStamp, $nonce);
+
+        //生成发送的xml
+        $encryptMsg = $this->_generateXml($encrypt, $signature, $timestamp, $nonce);
+
         return [static::OK, $encryptMsg];
     }
 
 
     /**
-     * 消息解密
-     *
-     * @param $msgSignature
-     * @param $timeStamp
-     * @param $nonce
-     * @param $postData
-     * @return array
-     */
-    public function decryptMsg($msgSignature, $timeStamp, $nonce, $postData)
-    {
-        if (strlen($this->encodingAesKey) != 43) {
-            return [static::ILLEGAL_AES_KEY, null];
-        }
-        $pc = new Prpcrypt($this->encodingAesKey);
-        $array = $this->_extractXml($postData);
-        if ($array[0] != 0) {
-            return $array;
-        }
-        $timeStamp = $timeStamp ?: time();
-        $encrypt = $array[1];
-        $array = $this->_getSHA1($this->token, $timeStamp, $nonce, $encrypt);
-        if ($array[0] != 0) {
-            return $array;
-        }
-        $signature = $array[1];
-        if ($signature != $msgSignature) {
-            return [static::VALIDATE_SIGNATURE_ERROR, null];
-        }
-        $result = $pc->decrypt($encrypt, $this->appId);
-        if ($result[0] != 0) {
-            return $result;
-        }
-        return [static::OK, $result[1]];
-    }
-
-
-    /**
-     * 明文加密
-     *
-     * @param $text
-     * @param $appid
-     * @return array
-     */
-    private function _encrypt($text, $appid)
-    {
-        try {
-            $random = $this->_getRandomStr();
-            $text = $random . pack("N", strlen($text)) . $text . $appid;
-            $module = mcrypt_module_open(MCRYPT_RIJNDAEL_128, '', MCRYPT_MODE_CBC, '');
-            $iv = substr($this->encodingAesKey, 0, 16);
-            $text = $this->_encode($text);
-            mcrypt_generic_init($module, base64_decode($this->encodingAesKey . "="), $iv);
-            $encrypted = mcrypt_generic($module, $text);
-            mcrypt_generic_deinit($module);
-            mcrypt_module_close($module);
-            return [static::OK, base64_encode($encrypted)];
-        } catch (\Exception $e) {
-            return [static::ENCRYPT_AES_ERROR, null];
-        }
-    }
-
-
-    /**
-     * 明文解密
-     *
-     * @param $encrypted
-     * @param $appid
-     * @return array
-     */
-    private function _decrypt($encrypted, $appid)
-    {
-        try {
-            $ciphertext_dec = base64_decode($encrypted);
-            $module = mcrypt_module_open(MCRYPT_RIJNDAEL_128, '', MCRYPT_MODE_CBC, '');
-            $iv = substr($this->encodingAesKey, 0, 16);
-            mcrypt_generic_init($module, base64_decode($this->encodingAesKey . "="), $iv);
-            $decrypted = mdecrypt_generic($module, $ciphertext_dec);
-            mcrypt_generic_deinit($module);
-            mcrypt_module_close($module);
-        } catch (\Exception $e) {
-            return [static::DECRYPT_AES_ERROR, null];
-        }
-
-        try {
-            $result = $this->_decode($decrypted);
-            if (strlen($result) < 16) {
-                return "";
-            }
-            $content = substr($result, 16, strlen($result));
-            $len_list = unpack("N", substr($content, 0, 4));
-            $xml_len = $len_list[1];
-            $xml_content = substr($content, 4, $xml_len);
-            $from_appid = substr($content, $xml_len + 4);
-        } catch (\Exception $e) {
-            return [static::ILLEGAL_BUFFER, null];
-        }
-        if ($from_appid != $appid) {
-            return [static::VALIDATE_APPID_ERROR, null];
-        }
-        return [0, $xml_content];
-    }
-
-
-    /**
      * 对明文进行加密
-     *
      * @param string $text 需要加密的明文
      * @return string 加密后的密文
      */
     public function encrypt($text, $appid)
     {
         try {
-            $random = $this->_getRandomStr();
+            //获得16位随机字符串，填充到明文之前
+            $random = $this->getRandomStr();
             $text = $random . pack("N", strlen($text)) . $text . $appid;
-            $iv = substr($this->encodingAesKey, 0, 16);
-            $pkc_encoder = new PKCS7Encoder();
+
+            $iv = substr($this->key, 0, 16);
+            //使用自定义的填充方式对明文进行补位填充
+            $pkc_encoder = new PKCS7Encoder;
             $text = $pkc_encoder->encode($text);
-            $encrypted = openssl_encrypt($text, 'AES-256-CBC', substr($this->encodingAesKey, 0, 32), OPENSSL_ZERO_PADDING, $iv);
-            return array(ErrorCode::$OK, $encrypted);
+            $encrypted = openssl_encrypt($text, 'AES-256-CBC', $this->key, OPENSSL_RAW_DATA, $iv);
+            return array(ErrorCode::$OK, base64_encode($encrypted));
         } catch (\Exception $e) {
             return array(ErrorCode::$EncryptAESError, null);
         }
+    }
+
+
+    /**
+     * 消息解密
+     *
+     * @param string $signature
+     * @param string $timestamp
+     * @param string $nonce
+     * @param string $postData
+     * @return array|bool|string
+     */
+    public function decryptMsg(string $signature = '', string $timestamp = '', string $nonce = '', string $postData = '')
+    {
+        if (strlen($this->encodingAesKey) != 43) {
+            return [static::ILLEGAL_AES_KEY, null];
+        }
+
+        if (!$postData) {
+            return [static::PARSE_XML_ERROR, null];
+        }
+
+        //提取密文
+        $array = $this->_extractXml($postData);
+        if ($array[0] != 0) {
+            return $array;
+        }
+
+        $timestamp = $timestamp ? : time();
+
+        $encrypt = $array[1];
+
+        //验证安全签名
+        $array = $this->_getSHA1($this->token, (string)$timestamp, (string)$nonce);
+        if ($array[0] != 0) {
+            return $array;
+        }
+        $checkSignature = $array[1];
+        if ($signature != $checkSignature) {
+            return [static::VALIDATE_SIGNATURE_ERROR, null];
+        }
+
+        $result = $this->decrypt($encrypt, $this->appId);
+
+        if ($result[0] != 0) {
+            return $result;
+        }
+
+        return [static::OK, $result[1]];
     }
 
 
@@ -207,15 +160,19 @@ class MsgCrypt
     public function decrypt($encrypted, $appid)
     {
         try {
-            $iv = substr($this->encodingAesKey, 0, 16);
-            $decrypted = openssl_decrypt($encrypted, 'AES-256-CBC', substr($this->encodingAesKey, 0, 32), OPENSSL_ZERO_PADDING, $iv);
+            $iv = substr($this->key, 0, 16);
+            //使用BASE64对需要解密的字符串进行解码
+            $decrypted = openssl_decrypt(base64_decode($encrypted), 'AES-256-CBC', $this->key, OPENSSL_RAW_DATA, $iv);
         } catch (\Exception $e) {
             return array(ErrorCode::$DecryptAESError, null);
         }
 
         try {
-            $pkc_encoder = new PKCS7Encoder();
+            //去除补位字符
+            $pkc_encoder = new PKCS7Encoder;
             $result = $pkc_encoder->decode($decrypted);
+
+            //去除16位随机字符串,网络字节序和AppId
             if (strlen($result) < 16) {
                 return "";
             }
@@ -224,17 +181,18 @@ class MsgCrypt
             $xml_len = $len_list[1];
             $xml_content = substr($content, 4, $xml_len);
             $from_appid = substr($content, $xml_len + 4);
-            if (!$appid) {
-                $appid = $from_appid;
-            }
         } catch (\Exception $e) {
             return array(ErrorCode::$IllegalBuffer, null);
         }
         if ($from_appid != $appid) {
             return array(ErrorCode::$ValidateAppidError, null);
         }
-        return array(0, $xml_content, $from_appid);
+
+        return array(0, $xml_content);
     }
+
+
+
 
 
     /**
@@ -245,8 +203,11 @@ class MsgCrypt
      */
     private function _encode($text)
     {
+        //计算需要填充的位数
         $amount_to_pad = static::BLOCK_SIZE - (strlen($text) % static::BLOCK_SIZE);
-        $amount_to_pad = $amount_to_pad ?: static::BLOCK_SIZE;
+        $amount_to_pad = $amount_to_pad ? : static::BLOCK_SIZE;
+
+        //获得补位所用的字符
         $pad_chr = chr($amount_to_pad);
         $tmp = '';
         for ($index = 0; $index < $amount_to_pad; $index++) {
@@ -273,16 +234,21 @@ class MsgCrypt
 
 
     /**
-     * 对解密后的明文进行补位删除
+     * 计算签名
      *
-     * @return array
+     * @param string $token
+     * @param string $timestamp
+     * @param string $nonce
+     * @return array|bool
      */
-    private function _getSHA1()
+    private function _getSHA1(string $token, string $timestamp, string $nonce)
     {
         try {
-            $array = func_get_args();
-            sort($array, SORT_STRING);
-            return [static::OK, sha1(implode($array))];
+            $tmpArr = [$token, $timestamp, $nonce];
+            sort($tmpArr, SORT_STRING);
+            $tmpStr = implode($tmpArr);
+            $tmpStr = sha1($tmpStr);
+            return [static::OK, $tmpStr];
         } catch (\Exception $e) {
             return [static::COMPUTE_SIGNATURE_ERROR, null];
         }
@@ -291,13 +257,7 @@ class MsgCrypt
 
     private function _getRandomStr()
     {
-        $str = "";
-        $str_pol = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz";
-        $max = strlen($str_pol) - 1;
-        for ($i = 0; $i < 16; $i++) {
-            $str .= $str_pol[mt_rand(0, $max)];
-        }
-        return $str;
+        return substr(str_shuffle('ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz'), 0, 16);
     }
 
 
@@ -314,7 +274,7 @@ class MsgCrypt
             $array_e = $xml->getElementsByTagName('Encrypt');
             $encrypt = $array_e->item(0)->nodeValue;
             return [static::OK, $encrypt];
-        } catch (\Exception $e) {
+        } catch (Exception $e) {
             return [static::PARSE_XML_ERROR, null];
         }
     }
@@ -336,6 +296,24 @@ class MsgCrypt
         <Nonce><![CDATA[%s]]></Nonce>
         </xml>";
         return sprintf($format, $encrypt, $signature, $timestamp, $nonce);
+    }
+
+
+
+    /**
+     * 随机生成16位字符串
+     * @return string 生成的字符串
+     */
+    function getRandomStr()
+    {
+
+        $str = "";
+        $str_pol = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz";
+        $max = strlen($str_pol) - 1;
+        for ($i = 0; $i < 16; $i++) {
+            $str .= $str_pol[mt_rand(0, $max)];
+        }
+        return $str;
     }
 
 }
--- src/Services/WechatService.php
+++ src/Services/WechatService.php
@@ -29,8 +29,10 @@ class WechatService
      * WechatService constructor.
      * @param string $appId
      * @param string $appSecret
+     * @param string $token
+     * @param string $encodingAesKey
      */
-    public function __construct(string $appId = '', string $appSecret = '', string $token='', string $encodingAesKey='')
+    public function __construct(string $appId = '', string $appSecret = '', string $token = '', string $encodingAesKey = '')
     {
         $this->appId = $appId;
         $this->appSecret = $appSecret;
@@ -67,8 +69,10 @@ class WechatService
      */
     public function getTemplateList(string $accessToken)
     {
-        return $this->http->get("cgi-bin/template/get_all_private_template?access_token={$accessToken}", [
-            'form_params' => []
+        return $this->http->get("cgi-bin/template/get_all_private_template", [
+            'query' => [
+                'access_token' => $accessToken
+            ]
         ]);
     }
 
@@ -83,26 +87,89 @@ class WechatService
      */
     public function sendMessage(string $accessToken, array $data)
     {
-        return $this->http->get("cgi-bin/message/template/send?access_token={$accessToken}", [
-            'form_params' => [
-
+        return $this->http->post("cgi-bin/message/template/send", [
+            'query' => [
+                'access_token' => $accessToken
             ]
         ]);
     }
 
 
     /**
-     * 获取推送内容
+     * 发送客服消息
+     *
+     * @param string $accessToken
+     * @param array $message
+     * @return mixed
      */
-    public function getReceiveXml($postData = null, $getData = null)
+    public function customSend(string $accessToken, $message)
+    {
+        $url = "https://api.weixin.qq.com/cgi-bin/message/custom/send?access_token=" . $accessToken;
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_POST, 1);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, FALSE);
+        if (!empty($message)) {
+            curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($message, JSON_UNESCAPED_UNICODE));
+        }
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        $output = curl_exec($curl);
+        curl_close($curl);
+
+        return $output;
+    }
+
+
+    /**
+     * 获取推送内容
+     *
+     * @param string|null $postData
+     * @param array $getData
+     * @return mixed
+     */
+    public function getReceiveXml(string $postData = "", array $getData = [])
     {
         $crypter = new MsgCrypt($this->token, $this->encodingAesKey, $this->appId);
         $postData = $postData ? : file_get_contents("php://input");
         $getData = $getData ? : Request::input();
-        $result = $crypter->decryptMsg($getData['msg_signature'], $getData['timestamp'], $getData['nonce'], $postData);
-        return $result;
+        $result = $crypter->decryptMsg($getData['signature'], $getData['timestamp'], $getData['nonce'], (string)$postData);
+        if (isset($result[1])) {
+            return $this->fromXml($result[1]);
+        }
     }
 
-    
+
+
+    /**
+     * 获取用户信息
+     *
+     * @param string $accessToken
+     * @param string $openid
+     * @return mixed
+     */
+    public function getUserInfo(string $accessToken, string $openid)
+    {
+        return $this->http->get("/cgi-bin/user/info", [
+            'query' => [
+                'access_token' => $accessToken,
+                'openid' => $openid,
+                'lang' => "zh_CN"
+            ]
+        ]);
+    }
+
+
+
+    /**
+     * xml 转 array
+     * @param string $xml
+     * @return mixed
+     */
+    public function fromXml(string $xml = "") {
+        libxml_disable_entity_loader(true);
+        $this->values = json_decode(json_encode(simplexml_load_string($xml, 'SimpleXMLElement', LIBXML_NOCDATA)), true);
+        return $this->values;
+    }
 
 }
